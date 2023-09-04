7ca790f91ac8fb2e957ff65724215a0b1ca9bfc9
Emir <69427895+emirgl@users.noreply.github.com>
2023-09-04 11:24:41 +0300

    Update areaFinder.m

--- areaFinder.m
+++ areaFinder.m
@@ -1,1 +1,22 @@
 
+x = 0:0.01:5.5
+z = length(x)
+t = 1 
+k = 1
+
+while k <= z
+  if x(k) >= 1 & x(k) <= 4 & mod(x(k),1) == 0
+    t = t+1
+  elseif x(k) == 4.5
+    t = t+1
+  end
+    
+  y(k) = t  
+  
+k = k+1
+end 
+
+plot(x,y,'-r')
+grid on
+area = trapz(x,y)
+disp(area)
